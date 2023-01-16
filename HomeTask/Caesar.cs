818fc1d948d19fa2d450034ed2e9c0f05bd705a8
irfan <irfanfy@code.edu.az.com>
2023-01-17 02:32:09 +0400

    TimeCaesarTask

---
+++ HomeTask/Caesar.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_
+{
+    internal class Caesar
+    {
+        public static string CaesarDecode (int k, string v) 
+        {
+            Console.WriteLine("How many scrolls into left");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Insert Letter or Word: ");
+            string s = Console.ReadLine();
+            string result = string.Empty;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsUpper(s[i]))
+                {
+                    int b = (s[i] - 64 - a) % 26 + 64;
+
+                    if (b <= 64)
+                        b = b + 26;
+
+                    result += (char)b;
+                }
+
+                else
+                {
+                    int c = (s[i] - 96 - a) % 26 + 96;
+
+                    if(c <= 96)
+                        c = c + 26;
+
+                    result += (char)c;
+                }
+            }
+
+            return result;
+        }
+
+        
+    }
+}
---
+++ HomeTask/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace HomeTask_
+{
+    internal class Program
+    {
+       static void Main(string[] args)
+        {
+            #region TimeDifferenceTask
+
+            var timeDifference = new TimeDifference();
+            Console.Write("Starting time:");
+            string time1 = Console.ReadLine();
+            Console.Write("Finishing time:");
+            string time2 = Console.ReadLine();
+            string time3;
+            var result = timeDifference.DifferenceTime(time1, time2, out time3);
+            Console.WriteLine(result.ToString());
+
+            #endregion
+
+            #region CaesarTask
+
+            //Console.WriteLine(Caesar.CaesarDecode(1, "Wooo"));
+
+            #endregion
+
+        }
+
+
+    }
+}
